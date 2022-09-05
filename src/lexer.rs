16e157af6df13324a7d94055297504c811315487
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2022-09-05 19:06:00 +0200

    basic lexer and stack struct

---
+++ src/lexer.rs
@@ -0,0 +1,68 @@
+#![allow(dead_code)]
+#![allow(unused)]
+
+use crate::lexer::TYPES::TYPE;
+
+// GLOBALS
+static DIGITS: [&str; 10] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// FILE
+#[derive(Debug)]
+pub struct File {
+    pub name: String,
+    pub text: String
+}
+
+// TOKEN
+#[derive(Debug)]
+pub enum TYPES {
+    INT(isize), TYPE, SUB(Vec<Token>)
+}
+#[derive(Debug)]
+pub struct Token {
+    pub typ: TYPES,
+    pub start: usize,
+    pub stop: usize
+} impl Token {
+    pub fn new(typ: TYPES, start: usize, stop: usize) -> Self { Self { typ, start, stop } }
+}
+
+// LEXER
+#[derive(Debug)]
+pub struct Lexer {
+    tokens: Vec<Token>,
+    file: File,
+    idx: usize
+} impl Lexer {
+    pub fn new(mut file: File) -> Self { Self { tokens: Vec::new(), file, idx: 0 } }
+    pub fn advance(&mut self) { self.idx += 1 }
+    pub fn char(&self) -> &str {
+        if self.idx >= self.file.text.len() { return "" }
+        &self.file.text[self.idx..self.idx+1]
+    }
+    pub fn range(&self, start: usize, stop: usize) -> &str { &self.file.text[start..stop] }
+    pub fn next(&mut self) -> Result<(), String> {
+        if DIGITS.contains(&self.char()) {
+            let start = self.idx;
+            while DIGITS.contains(&self.char()) { self.advance(); }
+            let number = self.range(start, self.idx);
+            self.tokens.push(Token::new(TYPES::INT(number.parse::<isize>().unwrap()), start, self.idx));
+            return Ok(())
+        }
+        if self.char() == " " || self.char() == "\t" { self.advance(); return Ok(()) }
+        return Err(String::from(format!("CHAR ERROR: '{}'", self.char())))
+    }
+    pub fn lex(&mut self) -> Result<(), String> {
+        while self.char() != "" {
+            let res = self.next();
+            if res.is_err() { return Err(res.err().unwrap()) }
+        }
+        Ok(())
+    }
+}
+pub fn lex(name: String, text: String) -> Result<Vec<Token>, String> {
+    let mut lexer = Lexer::new(File{name, text});
+    let res = lexer.lex();
+    if res.is_err() { return Err(res.err().unwrap()) }
+    return Ok(lexer.tokens)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+#![allow(dead_code)]
+#![allow(unused)]
+use std::fs::File;
+use std::io::{BufRead, BufReader, ErrorKind};
+use std::process::exit;
+
+mod lexer;
+pub fn read_file(path: &str) -> String {
+    let f = match File::open(path) {
+        Ok(v) => v,
+        Err(e) => {
+            if e.kind() == ErrorKind::NotFound { eprintln!("couldn't find file path '{}'", path) }
+            else { eprintln!("{}", e) }
+            exit(1)
+        }
+    };
+    let mut lines = BufReader::new(f).lines();
+    let mut text = String::new();
+    loop {
+        if let Some(s) = lines.next() { text.push_str(s.unwrap().as_str()); }
+        else { break }
+        text.push_str("\n");
+    }
+    text.pop();
+    return text;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_name = String::from("test/test.rst");
+    let text = read_file(file_name.as_str());
+    let lex_res = lexer::lex(file_name, text);
+    if lex_res.is_err() { eprintln!("{}", lex_res.err().unwrap()); return }
+    let tokens = lex_res.unwrap();
+    for token in tokens { println!("{:?}", token); }
 }
